a4389ef2b8e847ec2684650a1c065cf1f91d0a57
diselyuti <dima.selyutin.03@gmail.com>
2023-12-03 23:01:20 +0200

    project done

---
+++ Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRSPO_final
+{
+    public class Client
+    {
+        private const int Port = 12345;
+        private const string ServerAddress = "127.0.0.1";
+        private const int MinMatrixSize = 10;
+
+        public void Run()
+        {
+            // Генерація та заповнення рандомними значеннями матриць NxM та MxL, де N,M,L >= 1000
+            var rnd = new Random();
+            var N = MinMatrixSize + rnd.Next(2);
+            var M = MinMatrixSize + rnd.Next(2);
+            var L = MinMatrixSize + rnd.Next(2);
+            var matrix1 = new MatrixRequestContainer(GenerateMatrix(N, M), true);
+            var matrix2 = new MatrixRequestContainer(GenerateMatrix(M, L), true);
+
+            // Встановлення з'єднання з сервером через TCP/IP
+            using (var client = new TcpClient(ServerAddress, Port))
+            using (var stream = client.GetStream())
+            {
+                var formatter = new BinaryFormatter();
+                // Передача матриць
+                formatter.Serialize(stream, matrix1);
+                formatter.Serialize(stream, matrix2);
+
+                // Отримання результату обчислень
+                var resultMatrix = (MatrixRequestContainer)formatter.Deserialize(stream);
+
+                if (!resultMatrix.ok)
+                {
+                    Console.WriteLine("Client message: " + resultMatrix.error);
+                    return;
+                }
+                
+                // Виведення в консоль результатів обчислень
+                DisplayMatrix(resultMatrix.matrix);
+            }
+        }
+
+        private int[,] GenerateMatrix(int rows, int cols)
+        {
+            var matrix = new int[rows, cols];
+            var rand = new Random();
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    matrix[i, j] = rand.Next(10);
+            return matrix;
+        }
+
+        private void DisplayMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($"{matrix[i, j]} ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRSPO_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запустити сервер в окремому потоці
+            Task serverTask = Task.Run(() => StartServer());
+
+            // Дати серверу час запуститися
+            System.Threading.Thread.Sleep(1000);
+
+            // Запустити кілька клієнтів для запиту на обчислення
+            Task.Run(() => StartClient());
+            Task.Run(() => StartClient());
+            System.Threading.Thread.Sleep(1000);
+            Task.Run(() => StartClient());
+
+
+            // Демонструємо можливість відправки запиту на обчислення при натисканні Enter в будь-який момент
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ReadLine();
+                Task.Run(() => StartClient());
+            }
+
+            Task.WaitAll(serverTask);
+        }
+
+        static void StartServer()
+        {
+            Console.WriteLine("Server started.");
+            var server = new Server();
+            server.Run();
+        }
+
+        static void StartClient()
+        {
+            Console.WriteLine("Client started.");
+            var client = new Client();
+            client.Run();
+        }
+    }
+}
